2a5f6fae7fff63e9071cf9232cbb3c5244985d71
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-12-01 14:18:15 -0700

    finish pset6

--- practice.py
+++ practice.py
@@ -1,92 +1,16 @@
-def bubble_sort(L):
-    swap = Flase
-    while not swap:
-        swap = True
-        for j in range(1, len(L)):
-            if L[j-1] > L[j]:
-                swap = False
-                temp = L[j]
-                L[j] = L[j-1]
-                L[j-1] = temp
-
-def selection_sort(L):
-    suffixSt = 0
-    while suffixSt != len(L):
-        for i in range(suffixSt, len(L)):
-            if L[i] < L[suffixSt]:
-                L[suffixSt], L[i] = L[i], L[suffixSt]
-        suffixSt += 1
-    return L
-
-def selSort(L):
-    for i in range(len(L) - 1):
-        minIndx = i
-        minVal = L[i]
-        j = i+1
-        while j < len(L):
-            if minVal > L[j]:
-                minIndx = j
-                minVal = L[j]
-            j += 1
-        if minIndx != i:
-            temp = L[i]
-            L[i] = L[minIndx]
-            L[minIndx] = temp
-    return L
-
-def newSort(L):
-    for i in range(len(L) - 1):
-        j=i+1
-        while j < len(L):
-            if L[i] > L[j]:
-                temp = L[i]
-                L[i] = L[j]
-                L[j] = temp
-            j += 1
-    return L
-
-def mySort(L):
-    """ L, list with unique elements """
-    clear = False
-    while not clear:
-        clear = True
-        for j in range(1, len(L)):
-            if L[j-1] > L[j]:
-                clear = False
-                temp = L[j]
-                L[j] = L[j-1]
-                L[j-1] = temp
-
-def merge(left, right):
-    result = []
-    i, j = 0, 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    while (i < len(left)):
-        result.append(left[i])
-        i += 1
-    while (j < len(right)):
-        result.append(right[j])
-        j += 1
-    return result
-
-def merge_sort(L):
-    if len(L) < 2:
-        return L[:]
-    else:
-        middle = len(L)//2
-        left = merge_sort(L[:middle])
-        right = merge_sort(L[middle:])
-        return merge(lef, right)
-
+def swapSort(L): 
+    """ L is a list on integers """
+    print("Original L: ", L)
+    for i in range(len(L)):
+        for j in range(len(L)):
+            if L[j] < L[i]:
+                # the next line is a short 
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j] 
+                print(L)
+    print("Final L: ", L)
 
 list = [5, 8, 0, 6, 4, 2, 21, 7, 9, 16]
+list_sort = [7, 10]
 
-print(selection_sort(list))
-print(selSort(list))
-print(newSort(list))
+print(swapSort(list))
