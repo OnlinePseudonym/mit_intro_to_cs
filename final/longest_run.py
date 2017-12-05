401cf88600c81ccce57264e320ac022be773bd22
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-12-05 00:33:53 -0700

    final commit

--- final/longest_run.py
+++ final/longest_run.py
@@ -31,7 +31,9 @@ def longest_run(L):
                 cur_int = e
     if len(cur_list) > len(longest_inc):
         longest_inc = cur_list
+
     cur_list = []
+
     for e in L:
         if len(cur_list) < 1:
             cur_list.append(e)
@@ -47,30 +49,39 @@ def longest_run(L):
                 cur_list = []
                 cur_list.append(e)
                 cur_int = e
+
     if len(cur_list) > len(longest_inc):
         longest_inc = cur_list
-    print(longest_dec)
-    print(longest_inc)
+
+    cur_list = []
+
     if len(longest_dec) == len(longest_inc):
+
         def list_index(list, sublist):
             """return index of sublist"""
             for ind in (i for i,e in enumerate(list) if e == sublist[0]):
                 if list[ind:ind+len(sublist)] == sublist:
                     return ind
+
         dec_ind = list_index(L, longest_dec)
         inc_ind = list_index(L, longest_inc)
+        
         if dec_ind < inc_ind:
             for e in longest_dec:
                 result += e
+
         else:
             for e in longest_inc:
                 result += e
+
     elif len(longest_dec) > len(longest_inc):
         for e in longest_dec:
             result += e
+
     else:
         for e in longest_inc:
             result += e
+
     return result
 
 L = [1, 2, 3, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
